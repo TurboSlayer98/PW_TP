e285e01f9d05f6c0dec819341cf8b9011fd2aebc
TurboSlayer98 <hffm98@hotmail.com>
2024-04-10 22:26:55 +0100

    10/04

--- js/cars.js
+++ js/cars.js
@@ -1,37 +1,64 @@
+class CarDisplay {
+  constructor(containerId) {
+    this.container = document.getElementById(containerId);
+  }
 
-const carr = 0;
-
-const atualizarCarros = async () => {
-    let strHtml = ``
-    const response = await fetch('../assets/json/cars.json')
-    const cars = await response.json()
-    for (const car of cars) {
-      strHtml += `
-            <div class="col-xl-3 col-md-6 mb-5 d-inline-flex justify-content-center">
-              <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="${car.Picture}" alt="${car.Brand} ${car.Model}">
-                <h4 class="card-title mb-4 mt-4">${car.Brand} ${car.Model}</h4>
-                <a href="#" class="btn btn-primary"  data-bs-toggle="modal" data-bs-target="#CarModal" onclick="carregaModal('${car.Brand}','${car.Model}','${car.Year}','${car.Plate}','${car.Color}','${car.Door_Number}','${car.Kilometers}')">Details</a>
-              </div>
-            </div>    
-                 `
+  async fetchCars() {
+    try {
+      const response = await fetch('../assets/json/cars.json');
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const cars = await response.json();
+      this.renderCars(cars);
+    } catch (error) {
+      console.error('Could not fetch cars:', error);
     }
-    document.getElementById("cardCarros").innerHTML = strHtml;
+  }
+
+  renderCars(cars) {
+    this.container.innerHTML = cars.map(car => `
+      <div class="col-xl-3 col-md-6 mb-5 d-inline-flex justify-content-center">
+        <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="${car.Picture}" alt="${car.Brand} ${car.Model}">
+          <h4 class="card-title mb-4 mt-4">${car.Brand} ${car.Model}</h4>
+          <button class="btn btn-primary" data-bs-toggle="modal" onclick="carDisplay.loadModal(${JSON.stringify(car)})">Details</button>
+        </div>
+      </div>
+    `).join('');
+  }
+
+  loadModal(car) {
+    const modalId = `CarModal${car.Plate}`;
+    const modalHtml = `
+      <div class="modal fade" id="${modalId}" tabindex="-1" role="dialog" aria-labelledby="${modalId}Label" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="${modalId}Label">${car.Brand} ${car.Model}</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">×</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <img src="${car.Picture}" alt="${car.Brand} ${car.Model}" class="img-fluid">
+              <p>Brand: ${car.Brand}</p>
+              <p>Model: ${car.Model}</p>
+              <p>Year: ${car.Year}</p>
+              <p>Plate: ${car.Plate}</p>
+              <p>Color: ${car.Color}</p>
+              <p>Doors: ${car.Door_Number}</p>
+              <p>Kilometers: ${car.Kilometers}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+
+    document.body.insertAdjacentHTML('beforeend', modalHtml);
+    $(`#${modalId}`).modal('show');
+  }
 }
 
-atualizarCarros();
-
-function carregaModal(brand,model,year,plate,color,door_Number,kilometers) {
-    
-    document.getElementById("modal_Title").innerHTML = brand + ' ' + model;
-
-    //document.getElementById("carImg").src = picture;
-
-    document.getElementById("brand-p").innerHTML += brand;
-    document.getElementById("model-p").innerHTML += model;
-    document.getElementById("year-p").innerHTML += year;
-    document.getElementById("plate-p").innerHTML += plate;
-    document.getElementById("color-p").innerHTML += color;
-    document.getElementById("door-p").innerHTML += door_Number;
-    document.getElementById("kms-p").innerHTML += kilometers;
-}
+const carDisplay = new CarDisplay('cardCarros');
+carDisplay.fetchCars();
